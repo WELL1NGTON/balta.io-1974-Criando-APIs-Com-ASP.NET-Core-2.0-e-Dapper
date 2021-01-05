ed9298d240f0cc98d446ea6423d410e07b1072d8
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-05 16:04:36 -0300

    19. Exceptions e Validações

--- BaltaStore.Domain/StoreContext/Entities/Order.cs
+++ BaltaStore.Domain/StoreContext/Entities/Order.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using BaltaStore.Domain.StoreContext.Enums;
+using FluentValidator;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
-    public class Order
+    public class Order : Notifiable
     {
         private readonly IList<OrderItem> _items;
         private readonly IList<Delivery> _deliveries;
@@ -40,7 +41,9 @@ namespace BaltaStore.Domain.StoreContext.Entities
         {
             // Gera o número do pedido
             Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8).ToUpper();
-            // Validar
+
+            if (_items.Count == 0)
+                AddNotification("Order", "Este pedido não possui itens");
         }
 
         // Pagar um pedido
--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -21,13 +21,16 @@ namespace BaltaStore.Tests
             var cadeira = new Product("Cadeira", "Cadeira", "image.png", 559.90M, 10);
 
             var order = new Order(customer);
-            order.AddItem(new OrderItem(mouse, 5));
-            order.AddItem(new OrderItem(teclado, 5));
-            order.AddItem(new OrderItem(cadeira, 5));
-            order.AddItem(new OrderItem(impressora, 5));
+            // order.AddItem(new OrderItem(mouse, 5));
+            // order.AddItem(new OrderItem(teclado, 5));
+            // order.AddItem(new OrderItem(cadeira, 5));
+            // order.AddItem(new OrderItem(impressora, 5));
 
             // Realizei o pedido
             order.Place();
+
+            // Verificar se o pedido é valido
+            var valid = order.IsValid;
 
             // Simular o pagamento
             order.Pay();
