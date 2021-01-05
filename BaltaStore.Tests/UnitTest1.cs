02e6203f759b1741690b779e3707e6cb010dea87
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-05 13:57:46 -0300

    13. Corrupção no Código

--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using BaltaStore.Domain.StoreContext.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BaltaStore.Tests
@@ -8,6 +9,15 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            var c = new Customer(
+                "André",
+                "Baltieri",
+                "12345678911",
+                "hello@balta.io",
+                "1999999999",
+                "Rua dos Developers, 1024");
+
+            // c.FirstName = "";
         }
     }
 }
