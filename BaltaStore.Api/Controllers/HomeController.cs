7249a13492c09ffcb3673afb5bad607c2bd70940
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-06 14:18:36 -0300

    47. Padrão REST

--- BaltaStore.Api/Controllers/HomeController.cs
+++ BaltaStore.Api/Controllers/HomeController.cs
@@ -5,10 +5,35 @@ namespace BaltaStore.Api.Controllers
 
     public class HomeController : Controller
     {
+        [HttpGet]
         [Route("")]
+        // [Route("clientes")] // Listar todos os clientes
+        // [Route("clientes/2587")] // Listar o cliente 2587
+        // [Route("clientes/2587/pedidos")] // Pedidos do cliente 2587
         public string Get()
         {
-            return "Hello World";
+            return new { version = "Version 0.0.1" };
         }
+
+        // [HttpPost]
+        // [Route("rota/01")]
+        // public string Post()
+        // {
+        //     return "Hello World";
+        // }
+
+        // [HttpPut]
+        // [Route("rota/01")]
+        // public string Put()
+        // {
+        //     return "Hello World";
+        // }
+
+        // [HttpDelete]
+        // [Route("rota/01")]
+        // public string Delete()
+        // {
+        //     return "Hello World";
+        // }
     }
 }
