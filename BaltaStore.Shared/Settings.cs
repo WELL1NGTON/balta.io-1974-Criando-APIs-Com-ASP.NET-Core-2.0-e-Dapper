965615cfff6969765634c13069758249090ef086
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-06 12:05:06 -0300

    41. Criando o Data Context

---
+++ BaltaStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace BaltaStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = "server=localhost,1433;database=baltastore;User ID=SA;Password=2ae5FRj3";
+    }
+}
