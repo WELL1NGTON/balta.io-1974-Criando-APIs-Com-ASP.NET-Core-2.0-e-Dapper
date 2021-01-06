c2532e7c689834ab72aaa0d70e126d00fece2dfc
WELL1NGTON <wellingtonmassola@gmail.com>
2021-01-06 14:12:46 -0300

    46. Criando um Controller

---
+++ BaltaStore.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaltaStore.Api.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,7 +17,7 @@ namespace BaltaStore.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
